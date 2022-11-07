f9be9c5a8c845c8e8f24179a30cc6aa40d200329
Logan Lambert Scholfield <logan.scholfield@uvm.edu>
2022-11-07 16:37:43 -0500

    added new funciton

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def establish_connection(database_name):
     conn = sql.connect(database_name)
     cursor = conn.cursor()
     return conn, cursor
+
+def do_query(cursor, table_name, column_name):
+    data = cursor.execute(f'''SELECT '{column_name}' FROM '{table_name}';''').fetchall()
+    formated_data = ""
+    for x in data:
+        formated_data += x
+        formated_data += '\n'
